d30ea726efa00f90a72622fe3c2f3d0ad1dd98cc
John Sevdal <john.sevdal@swarco.com>
2023-08-28 12:51:02 +0200

    add some installers for plugins

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+wget --show-progress -o /dev/null -O- 'https://raw.githubusercontent.com/hyperupcall/autoenv/master/scripts/install.sh' | sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp ./.zshrc ~
 
