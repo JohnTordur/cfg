f44c2f83810265cad3ebf29ca239b503c3b90515
John Sevdal <john.sevdal@swarco.com>
2023-08-28 12:42:37 +0200

    add powerlevel10k installation

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp ./.zshrc ~
 
